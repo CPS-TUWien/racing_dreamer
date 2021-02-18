6e6981f5cb218cec316087127f5888a9c53c1508
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2021-02-18 11:01:36 +0100

    fix upload script

--- upload_logs.sh
+++ upload_logs.sh
@@ -3,8 +3,11 @@ psw=`cat .password`
 logdir=${1}
 
 echo -e "[Info] Creating tar ball for log dir ${logdir}\n"
-tarball="$(who | cut -d ' ' -f 1)_$(date '+%d%m%Y_%H%M%S')_$(basename -- ${logdir}).tar"
-tar cvf ${tarball} --exclude='*/videos' --exclude='*/checkpoints' ${logdir}
+tarball="${USER}_$(date '+%d%m%Y_%H%M%S')_$(basename -- ${logdir}).tar"
+
+# note: since tar v 1.29, exclude works only on successive arguments
+# for this reason we exclude checkpoints/*pkl after storing the best ones
+tar cvf ${tarball} --exclude='*/checkpoints' ${logdir}
 echo -e "[Info] Created ${tarball}\n"
 
 echo "[Info] Uploading ${tarball} ..."
