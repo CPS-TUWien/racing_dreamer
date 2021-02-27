610c20765764fb67c2b7ed0cbe62565c9847a61c
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2021-02-27 15:32:01 +0100

    fix logging bug

--- racing/tuning/objective.py
+++ racing/tuning/objective.py
@@ -7,12 +7,12 @@ from racing.tuning.util import get_params
 
 def objective(trial: Trial, args):
     args.params = get_params(trial=trial, tunable_params=args.tunable_params, default_params=args.default_params)
-    experiment, agent_ctor = dispatch_experiment(args, args.logdir)
+    experiment, agent_ctor = dispatch_experiment(args, f'{args.logdir}/{trial.number}')
     agent = experiment.configure_agent(agent_ctor)
     objective_value = -1e9
     for epoch in range(1, args.epochs+1):
         objective_value = experiment.run_trial(agent=agent, steps=args.steps)
-        print(f'Trial: {trial.number}: {objective_value} in epoch {epoch}')
+        print(f'Trial-{trial.number}: {objective_value} in epoch {epoch}')
         if not np.isnan(objective_value):
             trial.report(objective_value, step=epoch)
 
