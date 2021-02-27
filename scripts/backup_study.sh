27145fbdb9a1f63ff8d212f7519a2d41d4da5543
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2021-02-27 23:09:22 +0100

    reduce search space

--- scripts/backup_study.sh
+++ scripts/backup_study.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker exec $1 /usr/bin/mysqldump -u root --password=password mpo-study > $1.sql
+database=$1
+file=$2
+docker exec docker_database_1 /usr/bin/mysqldump -u root --password=password $database > $file
